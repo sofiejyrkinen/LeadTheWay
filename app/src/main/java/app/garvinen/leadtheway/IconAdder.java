a168131577c9dca2854816c397ad2e4124e9af47
sofiejyrkinen <sofie.jyrkinen@gmail.com>
2016-12-05 11:51:25 +0100

    Lagt till xml-filer och ändrat i några av dem

---
+++ app/src/main/java/app/garvinen/leadtheway/IconAdder.java
@@ -0,0 +1,13 @@
+package app.garvinen.leadtheway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IconAdder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icon_adder);
+    }
+}
---
+++ app/src/main/java/app/garvinen/leadtheway/NotificationAdder.java
@@ -0,0 +1,13 @@
+package app.garvinen.leadtheway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationAdder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_adder);
+    }
+}
