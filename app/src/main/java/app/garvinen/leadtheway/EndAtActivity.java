d7276ea546c83871f1b3631a825feba7995c925d
sofiejyrkinen <sofie.jyrkinen@gmail.com>
2016-12-06 15:01:17 +0100

    Förändringar i filerna EndAt.., GoToo & SettingsNot.java även i de tillhörande xml-filerna

--- app/src/main/java/app/garvinen/leadtheway/EndAtActivity.java
+++ app/src/main/java/app/garvinen/leadtheway/EndAtActivity.java
@@ -3,11 +3,15 @@ package app.garvinen.leadtheway;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TimePicker;
 
 public class EndAtActivity extends AppCompatActivity {
 
 
     private static String LOG_TAG = EndAtActivity.class.getName();
+    private TimePicker timePicker;
+    private int hour;
+    private int min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,9 @@ public class EndAtActivity extends AppCompatActivity {
         setContentView(R.layout.activity_end_at);
         Log.d(LOG_TAG, "Setting Layout Done");
         //initiateButtons();
+
+        timePicker = (TimePicker) findViewById(R.id.endAtTime);
+        timePicker.setIs24HourView(true);
     }
 
 
--- app/src/main/java/app/garvinen/leadtheway/GoTooActivity.java
+++ app/src/main/java/app/garvinen/leadtheway/GoTooActivity.java
@@ -3,11 +3,16 @@ package app.garvinen.leadtheway;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TimePicker;
 
 
 public class GoTooActivity extends AppCompatActivity {
 
+
     private static String LOG_TAG = GoTooActivity.class.getName();
+    private TimePicker timePicker;
+    private int hour;
+    private int min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,20 @@ public class GoTooActivity extends AppCompatActivity {
         setContentView(R.layout.activity_go_too);
         Log.d(LOG_TAG, "Setting Layout Done");
         //initiateButtons();
+
+        timePicker = (TimePicker) findViewById(R.id.goTooTime);
+        timePicker.setIs24HourView(true);
     }
 
     public void onStart(){
         super.onStart();
         Log.d(LOG_TAG, "onStart: ");
     }
+
+
+
+
+
 
 
 }//end of GoToActivity
