e6669f4d861036c69f37a58a11635ab1a159a853
sofiejyrkinen <sofie.jyrkinen@gmail.com>
2016-12-12 15:34:12 +0100

    Påbörjade skapandet av "din resa" aktiviteten.

--- app/src/main/java/app/garvinen/leadtheway/TravelInfoActivity.java
+++ app/src/main/java/app/garvinen/leadtheway/TravelInfoActivity.java
@@ -1,13 +1,14 @@
 package app.garvinen.leadtheway;
 
+import android.app.ListActivity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
-public class TravelInfoActivity extends AppCompatActivity {
+public class TravelInfoActivity extends ListActivity {
 
 
     private static String LOG_TAG = TravelInfoActivity.class.getName();
@@ -19,6 +20,10 @@ public class TravelInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_travel_info);
         Log.d(LOG_TAG, "Setting Layout Done");
         initiateButtons();
+
+        String [] travel = {"Dag","Fordonstyp", "Hållplats", "Läge", "Avgångstid", "Ankomsttid"};
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getListView().getContext(),android.R.layout.simple_list_item_1,travel);
+        getListView().setAdapter(adapter);
 
     } //end of onCreate
 
